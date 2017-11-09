856f735f6ac55e6b4c411be06a139ac8e8111d53
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-09 18:24:49 -0500

    the barest of bones

---
+++ Source/Part1/maxwell_griffin_lab4p1.cu
@@ -0,0 +1,14 @@
+/***
+ * File: maxwell_griffin_lab4p1.cu
+ * Desc: Multiplies a matrix with the transpose of itself, as an exercise in
+ *       writing a simple CUDA kernel and comparing it with its complementary
+ *       serial implementation
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+   return 0;
+}
