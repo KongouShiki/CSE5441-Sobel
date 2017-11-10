95162cbc165679b13c467c68dd8195535a2b7377
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-09 19:01:04 -0500

    Serial implementation following snippet given in assignment

--- Source/Part1/maxwell_griffin_lab4p1.cu
+++ Source/Part1/maxwell_griffin_lab4p1.cu
@@ -5,10 +5,45 @@
  *       serial implementation
  */
 
-#include <stdio.h>
+#include <stdbool>
+#include <stdlib.h>
+#include <time.h>
+
+#define MATRIX_DIM 1024
+
+/*
+ * Sets values in an x by y matrix to random values between 1.0 and 2.0
+ */
+void InitMatrixToRandomValues(double *matrix, int dimX, int dimY)
+{
+   // see the RNG on first call to this function only
+   static bool seeded = false;
+   if(!seeded)
+   {
+      seeded = true;
+      srand(time(NULL));
+   }
+
+   for(int y = 0; y < dimY; y++)
+      for(int x = 0; x < dimX; x++)
+         matrix[(y * dimY) + x] = ((double)rand() / RAND_MAX) + 1.0;
+}
 
 int main()
 {
+   /*
+    * Serial algorithm to multiply the matrix with its transpose
+    */
+
+   double A[MATRIX_DIM][MATRIX_DIM];
+   double C[MATRIX_DIM][MATRIX_DIM] = {{0}};    // static initialize to 0
+
+   InitMatrixToRandomValues(A);
+
+   for(int i = 0; i < MATRIX_DIM; i++)
+      for(int j = 0; j < MATRIX_DIM; j++)
+         for(int k = 0; k < MATRIX_DIM; k++)
+            C[i][j] += A[k][i] * A[k][j]
 
    return 0;
 }
