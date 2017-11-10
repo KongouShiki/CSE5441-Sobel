0473dbce72967379fc33f9a8092d2bb2b1206dc9
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-10 12:21:44 -0500

    refactor serial algorithm into function

--- Source/Part1/maxwell_griffin_lab4p1.cu
+++ Source/Part1/maxwell_griffin_lab4p1.cu
@@ -13,10 +13,13 @@
 
 /*
  * Sets values in an x by y matrix to random values between 1.0 and 2.0
+ * @param matrix -- a matrix
+ * @param rows -- number of rows in matrix
+ * @param cols -- number of columns in matrix
  */
-void InitMatrixToRandomValues(double *matrix, int dimX, int dimY)
+void InitMatrixToRandomValues(double *matrix, int rows, int cols)
 {
-   // see the RNG on first call to this function only
+   // seed the RNG on first call to this function only
    static bool seeded = false;
    if(!seeded)
    {
@@ -24,25 +27,35 @@ void InitMatrixToRandomValues(double *matrix, int dimX, int dimY)
       srand(time(NULL));
    }
 
-   for(int y = 0; y < dimY; y++)
-      for(int x = 0; x < dimX; x++)
-         matrix[(y * dimY) + x] = ((double)rand() / RAND_MAX) + 1.0;
+   for(int col = 0; col < cols; col++)
+      for(int row = 0; row < rows; row++)
+         matrix[(col * cols) + row] = ((double)rand() / RAND_MAX) + 1.0;
+}
+
+/*
+ * Serial algorithm to multiply a square matrix with its transpose
+ * @param inputMatrix -- the matrix to perform the multiplication on
+ * @param resultMatrix -- stores the result
+ * @param dim -- the size of each row and column
+ */
+void SerialMultiplyMatrixByTranspose(
+   double *inputMatrix,
+   double *resultMatrix,
+   int dim)
+{
+   for(int i = 0; i < dim; i++)
+      for(int j = 0; j < dim; j++)
+         for(int k = 0; k < dim; k++)
+            resultMatrix[i][j] += A[k][i] * A[k][j];
 }
 
 int main()
 {
-   /*
-    * Serial algorithm to multiply the matrix with its transpose
-    */
-   double A[MATRIX_DIM][MATRIX_DIM];
-   static double C[MATRIX_DIM][MATRIX_DIM];    // static initialize to 0
+   double A_serial[MATRIX_DIM][MATRIX_DIM];
+   static double C_serial[MATRIX_DIM][MATRIX_DIM];    // static initialize to 0
 
-   InitMatrixToRandomValues(&A[0][0], MATRIX_DIM, MATRIX_DIM);
-
-   for(int i = 0; i < MATRIX_DIM; i++)
-      for(int j = 0; j < MATRIX_DIM; j++)
-         for(int k = 0; k < MATRIX_DIM; k++)
-            C[i][j] += A[k][i] * A[k][j];
+   InitMatrixToRandomValues(&A_serial[0][0], MATRIX_DIM, MATRIX_DIM);
+   SerialMultiplyMatrixByTranspose(A_serial, C_serial, MATRIX_DIM);
 
    return 0;
 }
