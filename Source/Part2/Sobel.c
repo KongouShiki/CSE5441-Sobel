4cb12e34d256ea595e7694630184eabf611ee509
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-12 12:50:41 -0500

    restructure const Gx and Gy stencils

--- Source/Part2/Sobel.c
+++ Source/Part2/Sobel.c
@@ -5,32 +5,42 @@
 #include <math.h>
 #include "Sobel.h"
 
-static const Stencil_t Gx = {
-   .top =    { -1, 0, 1 },
-   .middle = { -2, 0, 2 },
-   .bottom = { -1, 0, 1 }
+static const double Gx_data[3][3] = {
+   { -1, 0, 1 },
+   { -2, 0, 2 },
+   { -1, 0, 1 },
 }
+static const Stencil_t Gx = {
+   .top = Gx_data[0],
+   .middle = Gx_data[1],
+   .bottom = Gx_data[2]
+};
 
+static const double Gy_data[3][3] = {
+   {  1,  2,  1 },
+   {  0,  0,  2 },
+   { -1, -2, -1 },
+}
 static const Stencil_t Gy = {
-   .top =    {  1,  2,  1 },
-   .middle = {  0,  0,  0 },
-   .bottom = { -1, -2, -1 }
+   .top = Gy_data[0],
+   .middle = Gy_data[1],
+   .bottom = Gy_data[2]
 }
 
 double Sobel_Magnitude(Stencil_t *stencil)
 {
-   double sumGx = 0, sumGy = 0;
+   double Gx_sum = 0, Gy_sum = 0;
 
    for(int i = 0; i < 3; i++)
    {
-      sumGx += (Gx.top[i] * stencil.top[i])
+      Gx_sum += (Gx.top[i] * stencil.top[i])
             +  (Gx.middle[i] * stencil.middle[i])
             +  (Gx.bottom[i] * stencil.bottom[i]);
 
-      sumGy += (Gy.top[i] * stencil.top[i])
+      Gy_sum += (Gy.top[i] * stencil.top[i])
             +  (Gy.middle[i] * stencil.middle[i])
             +  (Gy.bottom[i] * stencil.bottom[i]);
    }
 
-   return sqrt(pow(sumGx, 2) + pow(sumGy, 2));
+   return sqrt(pow(Gx_sum, 2) + pow(Gy_sum, 2));
 }
