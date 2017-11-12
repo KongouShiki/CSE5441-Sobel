0364385e51f49c54d4f3a7cdb277b24fc380029f
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-12 15:42:41 -0500

    Read command line args and alloc buffers

--- Source/Part2/maxwell_griffin_lab4p2.cu
+++ Source/Part2/maxwell_griffin_lab4p2.cu
@@ -4,15 +4,23 @@
  *       serial algorithm, and once by a massively parallel CUDA algorithm.
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <math.h>
 #include "Stencil.h"
 
+#define LINEARIZE(row, col, dim) \
+   (((row) * (dim)) + (col))
+
+/*
+ * Globals
+ */
 static double Gx_data[3][3] = {
    { -1, 0, 1 },
    { -2, 0, 2 },
    { -1, 0, 1 }
 };
+
 static Stencil_t Gx = {
    .top = Gx_data[0],
    .middle = Gx_data[1],
@@ -24,13 +32,22 @@ static double Gy_data[3][3] = {
    {  0,  0,  2 },
    { -1, -2, -1 }
 };
+
 static Stencil_t Gy = {
    .top = Gy_data[0],
    .middle = Gy_data[1],
    .bottom = Gy_data[2]
 };
 
-double Sobel_Magnitude(Stencil_t *stencil)
+
+/*
+ * Calculates the magnitude of the gradient of the stencil of image values,
+ * following the Sobel edge detection operator
+ *
+ * @param stencil -- the 3x3 image stencil
+ * @return -- magnitude of the gradient
+ */
+double SobelPixelMagnitude(Stencil_t *stencil)
 {
    double Gx_sum = 0, Gy_sum = 0;
 
@@ -52,15 +69,31 @@ double Sobel_Magnitude(Stencil_t *stencil)
 /*
 * Main function.
 */
-int main()
+int main(int argc, char* argv[])
 {
-   double x[3][3] = {{3,2,1}, {6,5,4}, {9,8,7}};
-   Stencil_t stencil;
-   stencil.top = x[0];
-   stencil.middle = x[1];
-   stencil.bottom = x[2];
+   // Check for correct number of comand line args
+   if (argc != 4)
+   {
+      printf("Error: Incorrect arguments.\n");
+      printf("Should be: <input.bmp> <serial_output.bmp> <cuda_output.bmp>\n");
+      return 0;
+   }
 
-   double result = Sobel_Magnitude(&stencil);
+   // Open the files specified by the command line args
+   FILE *inputFile = fopen(argv[1], "rb");
+   FILE *serialOutputFile = fopen(argv[2], "wb");
+   FILE *cudaOutputFile = fopen(argv[3], "wb");
+   if(inputFile == NULL)
+   {
+      printf("Error: %s could not be opened for reading.", argv[1]);
+   }
 
-   printf("The result of the edge detection is %lf\n", result);
+   // Read in input image and allocate space for new output image buffers
+   uint8_t *inputImage = (uint8_t *)read_bmp_file(inFile);
+	uint8_t *serialOutputImage = (uint8_t *)malloc(get_num_pixel());
+	uint8_t *cudaOutputImage = (uint8_t *)malloc(get_num_pixel());
+
+   // Free allocated memory
+   free(serialOutputImage);
+   free(cudaOutputImage);
 }
