d409ee29a9a15fb1553404b33829ad18dbd46033
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-13 17:57:55 -0500

    fix sobel alg to work with uint8_t's

--- Source/Part2/Sobel.c
+++ Source/Part2/Sobel.c
@@ -5,45 +5,32 @@
 #include <math.h>
 #include "Sobel.h"
 
-static double Gx_data[3][3] = {
+static int Gx[3][3] = {
    { -1, 0, 1 },
    { -2, 0, 2 },
    { -1, 0, 1 }
 };
 
-static Stencil_t Gx = {
-   .top = Gx_data[0],
-   .middle = Gx_data[1],
-   .bottom = Gx_data[2]
-};
-
-static double Gy_data[3][3] = {
+static int Gy[3][3] = {
    {  1,  2,  1 },
    {  0,  0,  2 },
    { -1, -2, -1 }
 };
 
-static Stencil_t Gy = {
-   .top = Gy_data[0],
-   .middle = Gy_data[1],
-   .bottom = Gy_data[2]
-};
-
 double Sobel_Magnitude(Stencil_t *stencil)
 {
-   double Gx_sum = 0, Gy_sum = 0;
+   double sumGx = 0, sumGy = 0;
 
-   int i;
-   for(i = 0; i < 3; i++)
+   for(int i = 0; i < 3; i++)
    {
-      Gx_sum += (Gx.top[i] * stencil->top[i])
-         +  (Gx.middle[i] * stencil->middle[i])
-         +  (Gx.bottom[i] * stencil->bottom[i]);
+      sumGx += (Gx[0][i] * stencil->top[i])
+         +  (Gx[1]][i] * stencil->middle[i])
+         +  (Gx[2][i] * stencil->bottom[i]);
 
-      Gy_sum += (Gy.top[i] * stencil->top[i])
-         +  (Gy.middle[i] * stencil->middle[i])
-         +  (Gy.bottom[i] * stencil->bottom[i]);
+      sumGy += (Gy[0][i] * stencil->top[i])
+         +  (Gy[1][i] * stencil->middle[i])
+         +  (Gy[2][i] * stencil->bottom[i]);
    }
 
-   return sqrt(pow(Gx_sum, 2) + pow(Gy_sum, 2));
+   return sqrt(pow(sumGx  2) + pow(sumGy, 2));
 }
